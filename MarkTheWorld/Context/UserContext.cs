6f1646cbc09dd412f97931bcd2ebbca90d478e1c
Leocanis <kuzminskast@gmail.com>
2015-12-05 17:36:31 +0200

    asdfasdf

---
+++ MarkTheWorld/Context/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MarkTheWorld.Context
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<MarkTheWorld.Models.User> Users { get; set; }
+    }
+}
